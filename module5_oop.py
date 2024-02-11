45bc01cbb9fefc74471b71e97573eeed1c21fe8a
Duoxi (Jacob) Xu <44653540+DuoxiXu@users.noreply.github.com>
2024-02-11 14:32:16 -0800

    Add files via upload

---
+++ module5_oop.py
@@ -0,0 +1,40 @@
+def user_input_check():
+    # Take the first input from user
+    loop_flag = True
+    input_msg = "Please input an integer"
+    while loop_flag:
+        try:
+            n_input = int(input(input_msg))
+            loop_flag = False
+        except:
+            input_msg = "Your input is invalid, please input an integer"
+
+    # Once we get the n_input, we ask the user to input n_input numbers again
+    input_list = []
+    for i in range(n_input):
+        loop_flag = True
+        input_msg = f"Please input No.{i+1} integer"
+        while loop_flag:
+            try:
+                input_list.append(int(input(input_msg)))
+                loop_flag = False
+            except:
+                input_msg = "Your input is invalid, please input an integer"
+
+    # Ask user to input again and check if it has been input before
+
+    loop_flag = True
+    input_msg = "Please input an integer for the system to check"
+    while loop_flag:
+        try:
+            rs_input = int(input(input_msg))
+            if rs_input in input_list:
+                print(input_list.index(rs_input)+1)
+            else:
+                print("-1")
+            loop_flag = False
+        except:
+            input_msg = "Your input is invalid, please input an integer"
+
+if __name__ == '__main__':
+    user_input_check()
